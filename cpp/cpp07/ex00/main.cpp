ff758caeafddbe37be4c71ef29f4fc4d3a186807
KimDae-hyun <dhkim5608@naver.com>
2021-12-05 20:54:48 +0900

    CPP Module 07 eval

--- cpp/cpp07/ex00/main.cpp
+++ cpp/cpp07/ex00/main.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daekim <daekim@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/05 20:00:12 by daekim            #+#    #+#             */
+/*   Updated: 2021/12/05 20:14:59 by daekim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Function.hpp"
 
 int main( void )
@@ -10,8 +22,8 @@ int main( void )
 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
 	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
 
-	std::string c = "chaine1";
-	std::string d = "chaine2";
+	std::string c = "daekim1";
+	std::string d = "daekim2";
 
 	::swap(c, d);
 	std::cout << "c = " << c << ", d = " << d << std::endl;
--- cpp/cpp07/ex01/iter.hpp
+++ cpp/cpp07/ex01/iter.hpp
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daekim <daekim@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/05 20:02:10 by daekim            #+#    #+#             */
+/*   Updated: 2021/12/05 20:12:57 by daekim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef ITER_HPP
 # define ITER_HPP
 
-#include <iostream>
+# include <iostream>
 
 template <typename T1>
-void iter(T1 *addr, size_t len, void(*fnp)(T1))
+void iter(T1 *addr, size_t len, void(*fnp)(T1 const &))
 {
     for (size_t i = 0; i < len; i++)
         fnp(addr[i]);
